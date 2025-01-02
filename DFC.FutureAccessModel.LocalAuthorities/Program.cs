ee4b9006a7390cee66e5cbf26997e57a48c626f4
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2025-01-02 14:03:47 +0000

    Uninstall package Microsoft.Azure.Functions.Extensions. Make the Program.cs class static.

--- DFC.FutureAccessModel.LocalAuthorities/Program.cs
+++ DFC.FutureAccessModel.LocalAuthorities/Program.cs
@@ -15,41 +15,46 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+namespace DFC.FutureAccessModel.LocalAuthorities
+{
+    internal static class Program
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddLogging();
-        services.AddSingleton<ILoggerHelper, LoggerHelper>();
-        services.AddSingleton<ISwaggerDocumentGenerator, SwaggerDocumentGenerator>();
-        services.AddSingleton<IManageLocalAuthorities, LocalAuthorityManagementFunctionAdapter>();
-        services.AddSingleton<ICreateDocumentClients, DocumentClientFactory>();
-        services.AddSingleton<ICreateLoggingContextScopes, LoggingContextScopeFactory>();
-        services.AddSingleton<ICreateValidationMessageContent, ValidationMessageContentFactory>();
-        services.AddSingleton<IProvideApplicationSettings, ApplicationSettingsProvider>();
-        services.AddSingleton<IProvideFaultResponses, FaultResponseProvider>();
-        services.AddSingleton<IProvideSafeOperations, SafeOperationsProvider>();
-        services.AddSingleton<IProvideStoragePaths, StoragePathProvider>();
-        services.AddSingleton<IStoreLocalAuthorities, LocalAuthorityStore>();
-        services.AddSingleton<IStoreDocuments, DocumentStore>();
-        services.AddSingleton<IValidateLocalAuthorities, LocalAuthorityValidator>();
-    })
-    .ConfigureLogging(logging =>
-    {
-        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
-        // For more information, see https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=windows#application-insights
-        logging.Services.Configure<LoggerFilterOptions>(options =>
+        private static async Task Main(string[] args)
         {
-            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
-                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-            if (defaultRule is not null)
-            {
-                options.Rules.Remove(defaultRule);
-            }
-        });
-    })
-    .Build();
+            var host = new HostBuilder()
+                .ConfigureFunctionsWebApplication()
+                .ConfigureServices(services =>
+                {
+                    services.AddApplicationInsightsTelemetryWorkerService();
+                    services.ConfigureFunctionsApplicationInsights();
+                    services.AddLogging();
+                    services.AddSingleton<ILoggerHelper, LoggerHelper>();
+                    services.AddSingleton<ISwaggerDocumentGenerator, SwaggerDocumentGenerator>();
+                    services.AddSingleton<IManageLocalAuthorities, LocalAuthorityManagementFunctionAdapter>();
+                    services.AddSingleton<ICreateDocumentClients, DocumentClientFactory>();
+                    services.AddSingleton<ICreateLoggingContextScopes, LoggingContextScopeFactory>();
+                    services.AddSingleton<ICreateValidationMessageContent, ValidationMessageContentFactory>();
+                    services.AddSingleton<IProvideApplicationSettings, ApplicationSettingsProvider>();
+                    services.AddSingleton<IProvideFaultResponses, FaultResponseProvider>();
+                    services.AddSingleton<IProvideSafeOperations, SafeOperationsProvider>();
+                    services.AddSingleton<IProvideStoragePaths, StoragePathProvider>();
+                    services.AddSingleton<IStoreLocalAuthorities, LocalAuthorityStore>();
+                    services.AddSingleton<IStoreDocuments, DocumentStore>();
+                    services.AddSingleton<IValidateLocalAuthorities, LocalAuthorityValidator>();
 
-host.Run();
+                    services.Configure<LoggerFilterOptions>(options =>
+                    {
+                        LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                            == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                        if (toRemove is not null)
+                        {
+                            options.Rules.Remove(toRemove);
+                        }
+                    });
+                })                
+                .Build();
+            
+            await host.RunAsync();
+        }
+    }
+}
